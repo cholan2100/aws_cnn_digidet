b228de305795d7699eb1db1564d449a7dbcf3ed1
g <g@nc>
2024-10-20 01:14:23 +0530

    download mnist

--- setup_env.sh
+++ setup_env.sh
@@ -9,6 +9,10 @@ source vitis_setup.sh
 echo "TODO: Setting up the Xilinx XRT environment..."
 echo "      This is painful and should be automated in the future."
 
+# run get_mnist.sh to get the MNIST data
+echo "Running get_mnist.sh to get the MNIST data..."
+./spooNN/mnist-cnn/deploy/get_mnist.sh
+
 # add NOTE to inform user about AWS configuration
 echo "Configure AWS credentials using 'aws configure' to use AWS FPGAs"
 
